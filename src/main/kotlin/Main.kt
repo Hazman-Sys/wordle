04c3ee8a59c48648d3aef4392e00947cf05e9a02
limbuserendipity <limbuserendipity@gmail.com>
2025-04-23 11:22:33 +0700

    add Square fun

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,9 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -7,17 +12,42 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
 fun App() {
-
+    Square()
 }
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         App()
     }
+}
+
+@Composable
+fun Square(){
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .size(148.dp)
+            .background(Color.LightGray)
+            .border(1.dp, Color.Gray, RoundedCornerShape(16.dp))
+            .clip(RoundedCornerShape(16.dp))
+    ){
+        Text(
+            text = "A",
+            fontSize = 47.sp
+        )
+    }
+
 }
