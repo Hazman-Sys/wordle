062e9a13fe30121dc0ad36f746444905b4521e2c
User <User123123@gmail.com>
2025-05-07 10:30:09 +0700

    third commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,11 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,38 +15,110 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.*
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.text.input.TextFieldValue
 
 @Composable
-@Preview
-fun App() {
-    Square()
+fun App(key: Char) {
+    println(key)
+
+    val words = listOf(
+        "Igloo",
+        "Sigma",
+        "House",
+        "World",
+        "Super",
+        "Class",
+        "Robot",
+        "Salve",
+        "Flank",
+        "Gauge",
+        "Gusto",
+        "Carom",
+        "Ouija"
+    )
+    val randomWord = words.random()
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .onKeyEvent {
+                println(Char(it.key.keyCode.toInt()))
+                false
+            }
+    ) {
+
+        val squaresList = (0..29).toList()
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(5),
+            modifier = Modifier.size(280.dp, 414.dp),
+        ) {
+            items(squaresList) { square ->
+
+
+//---------------------------------------------------------------------------------------------------
+                var textState by remember { mutableStateOf(TextFieldValue("")) }
+
+                Square(
+
+                ) {
+                    Text(
+                        text = "$key"
+                    )
+                }
+            }
+        }
+    }
 }
 
+
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+    var key by remember {
+        mutableStateOf(' ')
+    }
+    Window(
+        onCloseRequest = ::exitApplication,
+        onKeyEvent = { keyEvent ->
+            if(keyEvent.type == KeyEventType.KeyUp){
+                key = keyEvent.key.keyCode.toChar()
+            }
+            false
+        }
+    ) {
+        //App()
+        MaterialTheme {
+            Surface { App(
+                key = key
+            ) }
+        }
+
     }
 }
 
 @Composable
-fun Square(){
-
+fun Square(
+    content: @Composable () -> Unit
+) {
+    var color by remember {  mutableStateOf(Color(0xfbfcff)) }
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
-            .size(148.dp)
-            .background(Color.LightGray)
-            .border(1.dp, Color.Gray, RoundedCornerShape(16.dp))
-            .clip(RoundedCornerShape(16.dp))
-    ){
-        Text(
-            text = "A",
-            fontSize = 47.sp
-        )
+            .requiredSize(56.dp, 56.dp)
+            .padding(3.dp)
+            .clip(RoundedCornerShape(5.dp))
+            .background(color)
+            .border(2.dp, Color(0xffdee1e9), RoundedCornerShape(5.dp))
+            .padding(3.dp)
+    ) {
+        content()
     }
-
 }
+
+
+
+
